b6aefec5658cee4efac69d31951ed722741607ba
Melissa Albarella <moexu13@gmail.com>
2021-04-27 19:44:28 -0700

    Added Adobe fonts and favicon links

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,16 @@
+import Head from "next/head";
 import Header from "../components/Header";
 
 const Home = () => {
   return (
     <div className="wrapper">
+      <div className="head">
+        <Head>
+          {/* Adobe fonts */}
+          <link href="//use.typekit.net/sqt7nyn.css" rel="stylesheet" />
+          <link rel="icon" type="image/png" href="/images/logo.png" />
+        </Head>
+      </div>
       <Header />
     </div>
   );
