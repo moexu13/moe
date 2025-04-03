ce8da67fc2c8e0b163b348499b5c14e1402f6b62
Melissa Albarella <moexu13@gmail.com>
2025-04-03 12:39:28 -0400

    Add hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,18 @@
 import Header from "@/components/Header";
 import NavBar from "@/components/NavBar";
+import { loversQuarrel } from "./fonts";
 
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
       <NavBar />
+      <main>
+        <div className="flex flex-col items-center justify-center h-screen">
+          <h1 className={`text-8xl ${loversQuarrel.className}`}>Melissa Albarella</h1>
+          <p className="text-lg">Web Developer</p>
+        </div>
+      </main>
     </div>
   );
 }
