72a49dd78106a10fba1fbfb83d3339d992062baa
Melissa Albarella <71854802+moexu13@users.noreply.github.com>
2025-04-07 10:26:55 -0700

    Add portfolio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Header from "@/components/Header";
 import NavBar from "@/components/NavBar";
 import { loversQuarrel } from "./fonts";
 import TechBar from "@/components/TechBar";
+import Portfolio from "@/components/Portfolio";
 
 export default function Home() {
   return (
@@ -12,7 +13,9 @@ export default function Home() {
       <NavBar />
       <main className="w-full overflow-x-hidden pt-16 sm:pt-20 md:pt-32">
         <section className="flex flex-col items-center mt-8 sm:mt-16 md:mt-24 pb-4 px-4 min-h-[40vh]">
-          <h1 className={`text-4xl sm:text-5xl md:text-8xl ${loversQuarrel.className} text-center`}>Melissa Albarella</h1>
+          <h1 className={`text-4xl sm:text-5xl md:text-8xl ${loversQuarrel.className} text-center`}>
+            Melissa Albarella
+          </h1>
           <p className="text-sm md:text-base lg:text-lg text-center">Web Developer</p>
         </section>
         <section className="bg-(--color-primary) py-8 sm:py-12 md:py-16 flex flex-col justify-center items-center mt-6 min-h-[200px] md:min-h-[300px]">
@@ -22,6 +25,13 @@ export default function Home() {
             </div>
             <TechBar />
           </div>
+        </section>
+        <section id="portfolio" className="bg-(--color-secondary)/90 py-8 sm:py-12 md:py-16 flex flex-col justify-center items-center min-h-[200px] md:min-h-[300px]">
+          <section className="w-full flex justify-center items-center">
+            <div className="w-full max-w-[95%]">
+              <Portfolio />
+            </div>
+          </section>
         </section>
       </main>
     </div>
--- src/components/TechBox.tsx
+++ src/components/TechBox.tsx
@@ -7,9 +7,7 @@ interface TechBoxProps {
 
 const TechBox = ({ icon, label }: TechBoxProps) => {
   return (
-    <div
-      className="w-28 sm:w-32 md:w-36 h-24 sm:h-26 md:h-28 flex flex-col items-center px-3 md:px-4 py-3 md:py-4 rounded-lg bg-white/5 backdrop-blur-sm hover:bg-white/10 transition-colors self-center"
-    >
+    <div className="w-28 sm:w-32 md:w-36 h-24 sm:h-26 md:h-28 flex flex-col items-center px-3 md:px-4 py-3 md:py-4 rounded-lg bg-white/5 backdrop-blur-sm hover:bg-white/10 transition-colors self-center">
       <div className="h-2 md:h-3"></div>
       <div className="w-6 sm:w-7 md:w-8 h-6 sm:h-7 md:h-8 flex-shrink-0 flex items-center justify-center">
         <Image
