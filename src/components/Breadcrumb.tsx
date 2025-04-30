fbb697fefaa37e64c2b82ea7728278aae31f8c8f
Melissa Albarella <moexu13@gmail.com>
2025-04-29 21:45:47 -0400

    Extract svg components into separate files

--- src/components/Breadcrumb.tsx
+++ src/components/Breadcrumb.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { usePathname } from "next/navigation";
+import Image from "next/image";
 
 interface BreadcrumbProps {
   fixed?: boolean;
@@ -40,21 +41,13 @@ const Breadcrumb = ({ fixed = false }: BreadcrumbProps) => {
         {getBreadcrumbItems().map((item, index) => (
           <li key={item.href} className="inline-flex items-center">
             {index > 0 && (
-              <svg
-                className="w-3 h-3 text-white/40 mx-1"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 6 10"
-              >
-                <path
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="m1 9 4-4-4-4"
-                />
-              </svg>
+              <Image
+                src="/icons/chevron.svg"
+                alt=""
+                width={12}
+                height={12}
+                className="text-white/40 mx-1"
+              />
             )}
             {item.current ? (
               <span className="ml-1 text-sm font-medium text-white md:ml-2">{item.label}</span>
@@ -64,15 +57,7 @@ const Breadcrumb = ({ fixed = false }: BreadcrumbProps) => {
                 className="inline-flex items-center text-sm font-medium text-white/60 hover:text-white transition-colors cursor-pointer relative z-50"
               >
                 {index === 0 && (
-                  <svg
-                    className="w-3 h-3 mr-2.5"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                  >
-                    <path d="m19.707 9.293-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L2 10.414V18a2 2 0 0 0 2 2h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a2 2 0 0 0 2-2v-7.586l.293.293a1 1 0 0 0 1.414-1.414Z" />
-                  </svg>
+                  <Image src="/icons/home.svg" alt="" width={12} height={12} className="mr-2.5" />
                 )}
                 {item.label}
               </a>
